1c8614ccb07e1482fae5cb456e4cd646cec9daf6
mae <hiroki.mae@rakus.co.jp>
2017-08-14 19:37:34 +0900

    ログイン後、商品一覧と詳細画面でのユーザ名表示されるよう修正しました

--- src/main/java/jp/co/rakus/stockmanagement/App.java
+++ src/main/java/jp/co/rakus/stockmanagement/App.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class App {
 
+	
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
