5d902fdfd4afb1d6749d6d7ba3a0a4a89398b10a
mae <hiroki.mae@rakus.co.jp>
2017-08-14 19:35:51 +0900

    後、書籍一覧画面と書籍詳細画面に

--- src/main/java/jp/co/rakus/stockmanagement/web/LoginController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/LoginController.java
@@ -1,5 +1,7 @@
 package jp.co.rakus.stockmanagement.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.rakus.stockmanagement.domain.Member;
 import jp.co.rakus.stockmanagement.service.MemberService;
@@ -23,6 +26,8 @@ public class LoginController {
 
 	@Autowired
 	private MemberService memberService;
+	@Autowired
+	private HttpSession session;
 
 	/**
 	 * フォームを初期化します.
@@ -51,7 +56,7 @@ public class LoginController {
 	 */
 	@RequestMapping(value = "/login")
 	public String login(@Validated LoginForm form,
-			BindingResult result, Model model) {
+			BindingResult result,RedirectAttributes redirectAttributes, Model model) {
 		if (result.hasErrors()){
 			return index();
 		}
@@ -63,7 +68,7 @@ public class LoginController {
             result.addError(error);
 			return index();
 		}
-		model.addAttribute("member", member);
+		session.setAttribute("member", member);
 		return "redirect:/book/list";
 	}
 }
